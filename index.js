555f32890e36f57d303dadc313f203fa115a7cc3
Abdulla Malik <abdulla.coder@gmail.com>
2021-01-16 03:09:22 +0600

    ssl configured

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+var https = require("https");
 var proxy = require('html2canvas-proxy');
 var express = require('express');
+var helmet = require("helmet");
+
+const options = {
+    key: fs.readFileSync("/home/ubuntu/dev.xmock.com/privkey.pem"),
+    cert: fs.readFileSync("/home/ubuntu/dev.xmock.com/chain.pem"),
+    dhparam: fs.readFileSync("/home/ubuntu/dev.xmock.com/dh-strong.pem")
+  };
 
 var app = express();
+app.use(helmet());
 app.use('/', proxy());
 
-app.listen(3001, () => {
-    console.log(`Example app listening at http://localhost:3001`);
-});
+app.listen(3000);
+
+https.createServer(options, app).listen(3001);
