721c3009a7b5a40c201b9fc78fd36fc723abe13d
Abdulla Malik <abdulla.coder@gmail.com>
2021-01-16 22:41:01 +0600

    reverted back to original

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-var https = require("https");
-var fs = require("fs");
 var proxy = require('html2canvas-proxy');
 var express = require('express');
-var helmet = require("helmet");
-
-const options = {
-    key: fs.readFileSync("/etc/letsencrypt/live/dev.xmock.com/privkey.pem"),
-    cert: fs.readFileSync("/etc/letsencrypt/live/dev.xmock.com/chain.pem"),
-  };
 
 var app = express();
-app.use(helmet());
 app.use('/', proxy());
 
-app.listen(3000);
-
-https.createServer(options, app).listen(3001);
+app.listen(3001, () => {
+    console.log(`Example app listening at http://localhost:3001`);
+});
